6aad7a1ecf8b2cf5d6fc953faa0e146466d0c250
enesiyidil <enesiyidil16@gmail.com>
2024-01-25 19:17:18 +0300

    Create project and auth-service, account-service, sms service

---
+++ src/main/java/org/vem/vemfinancesolutions/VemFinanceSolutionsApplication.java
@@ -0,0 +1,13 @@
+package org.vem.vemfinancesolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VemFinanceSolutionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VemFinanceSolutionsApplication.class, args);
+    }
+
+}
